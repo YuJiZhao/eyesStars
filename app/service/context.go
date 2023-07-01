7da9e58a33efa368e866ce36ee07874e30218671
eyes <792734338@qq.com>
2023-07-01 22:15:39 +0800

    切换本地nacos

--- app/service/context.go
+++ app/service/context.go
@@ -21,6 +21,7 @@ type contextService struct {
 }
 
 var ContextService = contextService{
+	// 这也可以存到数据库里，然后持久化到内存中，再加个刷新缓存接口，以后再说。
 	initSiteIds: []uint32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
 }
 
